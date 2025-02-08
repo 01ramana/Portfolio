439deeba0024c46e8797eb46200917ccec12b03a
Ramana <01ramanareddy@gmail.com>
2025-02-08 09:13:36 +0530

    Initial commit

---
+++ Ramana_Reddy/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <div className="container">
+        <h2>About Me</h2>
+        <p>
+          Hi, I'm Nanchary Veera Venkata Ramana Reddy, a passionate developer pursuing a PG-DMC (Postgraduate Diploma in Management Consulting).
+          I specialize in Java, SQL, React, Node.js, Android, and many more technologies. I’m always excited to learn new tools and frameworks, and I strive to build impactful and innovative solutions.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
